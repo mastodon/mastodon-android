4615612a65b2362d4763e4ef2354e8f36ae79ff6
sk <sk22@mailbox.org>
2022-11-08 00:27:19 +0100

    use home_timeline string

--- mastodon/src/main/java/org/joinmastodon/android/fragments/SettingsFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/SettingsFragment.java
@@ -103,7 +103,7 @@ public class SettingsFragment extends MastodonToolbarFragment{
 			GlobalUserPreferences.save();
 		}));
 
-		items.add(new HeaderItem(R.string.settings_timeline));
+		items.add(new HeaderItem(R.string.home_timeline));
 		items.add(new SwitchItem(R.string.settings_show_replies, R.drawable.ic_fluent_chat_multiple_24_regular, GlobalUserPreferences.showReplies, i->{
 			GlobalUserPreferences.showReplies=i.checked;
 			GlobalUserPreferences.save();
