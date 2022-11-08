defa8b014e5096442f160290535ac560dc398f05
Grishka <git@grishka.me>
2022-11-08 09:38:26 +0300

    Fix #315

--- mastodon/src/main/java/org/joinmastodon/android/fragments/ComposeFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/ComposeFragment.java
@@ -394,16 +394,16 @@ public class ComposeFragment extends MastodonToolbarFragment implements OnBackPr
 			outState.putStringArrayList("pollOptions", opts);
 			outState.putInt("pollDuration", pollDuration);
 			outState.putString("pollDurationStr", pollDurationStr);
-			outState.putBoolean("hasSpoiler", hasSpoiler);
-			if(!attachments.isEmpty()){
-				ArrayList<Parcelable> serializedAttachments=new ArrayList<>(attachments.size());
-				for(DraftMediaAttachment att:attachments){
-					serializedAttachments.add(Parcels.wrap(att));
-				}
-				outState.putParcelableArrayList("attachments", serializedAttachments);
-			}
-			outState.putSerializable("visibility", statusVisibility);
 		}
+		outState.putBoolean("hasSpoiler", hasSpoiler);
+		if(!attachments.isEmpty()){
+			ArrayList<Parcelable> serializedAttachments=new ArrayList<>(attachments.size());
+			for(DraftMediaAttachment att:attachments){
+				serializedAttachments.add(Parcels.wrap(att));
+			}
+			outState.putParcelableArrayList("attachments", serializedAttachments);
+		}
+		outState.putSerializable("visibility", statusVisibility);
 	}
 
 	@Override
