1124486f1f27890abbc9172cc5dbe7f0e78e1eb9
FineFindus <63370021+FineFindus@users.noreply.github.com>
2023-01-26 20:56:15 +0100

    fix(Instance): typo langauges => languages

--- mastodon/src/main/java/org/joinmastodon/android/ui/tabs/TabLayout.java
+++ mastodon/src/main/java/org/joinmastodon/android/ui/tabs/TabLayout.java
@@ -602,7 +602,7 @@ public class TabLayout extends HorizontalScrollView {
    * <p>If the tab indicator color is not {@code Color.TRANSPARENT}, the indicator will be wrapped
    * and tinted right before it is drawn by {@link SlidingTabIndicator#draw(Canvas)}. If you'd like
    * the inherent color or the tinted color of a custom drawable to be used, make sure this color is
-   * set to {@code Color.TRANSPARENT} to avoid your color/tint being overriden.
+   * set to {@code Color.TRANSPARENT} to avoid your color/tint being overridden.
    *
    * @param color color to use for the indicator
    * @attr ref com.google.android.material.R.styleable#TabLayout_tabIndicatorColor
