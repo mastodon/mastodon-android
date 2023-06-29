cf8a9e18233a387bbb3dbd4823b615ef0e7fbcfa
Grishka <git@grishka.me>
2023-06-29 22:29:51 +0300

    Fix #609

--- mastodon/src/main/java/org/joinmastodon/android/ui/displayitems/MediaGridStatusDisplayItem.java
+++ mastodon/src/main/java/org/joinmastodon/android/ui/displayitems/MediaGridStatusDisplayItem.java
@@ -322,6 +322,7 @@ public class MediaGridStatusDisplayItem extends StatusDisplayItem{
 					altTextAnimator=null;
 					altTextWrapper.setVisibility(View.GONE);
 					btn.setVisibility(View.VISIBLE);
+					btn.setAlpha(1);
 				}
 			});
 			altTextAnimator=set;
