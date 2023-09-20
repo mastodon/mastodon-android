b89e0b5c5a2dd66feeeb7948f76d439ae4c3a519
LucasGGamerM <LucasGGamerM@protonmail.com>
2023-09-20 07:46:39 -0300

    fix(compose): fix photoPicker not popping up when there is less than 2 spaces available for media

--- mastodon/src/main/java/org/joinmastodon/android/fragments/ComposeFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/ComposeFragment.java
@@ -839,7 +839,8 @@ public class ComposeFragment extends MastodonToolbarFragment implements OnBackPr
 		boolean usePhotoPicker=UiUtils.isPhotoPickerAvailable();
 		if(usePhotoPicker){
 			intent=new Intent(MediaStore.ACTION_PICK_IMAGES);
-			intent.putExtra(MediaStore.EXTRA_PICK_IMAGES_MAX, mediaViewController.getMaxAttachments()-mediaViewController.getMediaAttachmentsCount());
+			if(mediaViewController.getMaxAttachments()-mediaViewController.getMediaAttachmentsCount()>1)
+				intent.putExtra(MediaStore.EXTRA_PICK_IMAGES_MAX, mediaViewController.getMaxAttachments()-mediaViewController.getMediaAttachmentsCount());
 		}else{
 			intent=new Intent(Intent.ACTION_GET_CONTENT);
 			intent.addCategory(Intent.CATEGORY_OPENABLE);
