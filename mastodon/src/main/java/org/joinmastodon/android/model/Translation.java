d5d12a7ce50d04888a0f4ae52de7a4c70afbc5e7
FineFindus <FineFindus@proton.me>
2023-11-01 19:56:59 +0100

    fix(status/translation): do not require all fields

--- mastodon/src/main/java/org/joinmastodon/android/model/Translation.java
+++ mastodon/src/main/java/org/joinmastodon/android/model/Translation.java
@@ -1,11 +1,14 @@
 package org.joinmastodon.android.model;
 
-import org.joinmastodon.android.api.AllFieldsAreRequired;
 
-@AllFieldsAreRequired
+import org.joinmastodon.android.api.RequiredField;
+
 public class Translation extends BaseModel{
+	@RequiredField
 	public String content;
+	@RequiredField
 	public String detectedSourceLanguage;
+	@RequiredField
 	public String provider;
 	public MediaAttachment[] mediaAttachments;
 
