c7b8cc72fc81a5640357b1bd69627b514a73bd2a
NorbiPeti <norbipeti@norbipeti.eu>
2024-05-01 23:14:08 +0200

    Display more user-friendly error messages
    Instead of displaying the Java
    exception, this change displays a more user-friendly message for some
    common network-related issues.
    Fixes mastodon/mastodon-android#667

--- mastodon/src/main/java/org/joinmastodon/android/api/MastodonErrorResponse.java
+++ mastodon/src/main/java/org/joinmastodon/android/api/MastodonErrorResponse.java
@@ -5,7 +5,13 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+
 import org.joinmastodon.android.R;
+
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 
 import me.grishka.appkit.api.ErrorResponse;
 
@@ -13,23 +19,36 @@ public class MastodonErrorResponse extends ErrorResponse{
 	public final String error;
 	public final int httpStatus;
 	public final Throwable underlyingException;
+	public final int messageResource;
 
 	public MastodonErrorResponse(String error, int httpStatus, Throwable exception){
 		this.error=error;
 		this.httpStatus=httpStatus;
 		this.underlyingException=exception;
+
+		if(exception instanceof UnknownHostException){
+			this.messageResource=R.string.could_not_reach_server;
+		}else if(exception instanceof SocketTimeoutException){
+			this.messageResource=R.string.connection_timed_out;
+		}else if(exception instanceof JsonSyntaxException || exception instanceof JsonIOException || httpStatus>=500){
+			this.messageResource=R.string.server_error;
+		}else if(httpStatus == 404){
+			this.messageResource=R.string.not_found;
+		}else{
+			this.messageResource=R.string.unknown_error;
+		}
 	}
 
 	@Override
 	public void bindErrorView(View view){
 		TextView text=view.findViewById(R.id.error_text);
-		text.setText(error);
+		text.setText(view.getContext().getString(messageResource, error));
 	}
 
 	@Override
 	public void showToast(Context context){
 		if(context==null)
 			return;
-		Toast.makeText(context, error, Toast.LENGTH_SHORT).show();
+		Toast.makeText(context, context.getString(messageResource, error), Toast.LENGTH_SHORT).show();
 	}
 }
