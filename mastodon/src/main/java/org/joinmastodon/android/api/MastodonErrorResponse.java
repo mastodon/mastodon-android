629e65edbad246f4c90dca20e20b9ff4f957bec8
NorbiPeti <norbipeti@norbipeti.eu>
2024-05-13 00:08:18 +0200

    Update error messages and remove the unknown error text

--- mastodon/src/main/java/org/joinmastodon/android/api/MastodonErrorResponse.java
+++ mastodon/src/main/java/org/joinmastodon/android/api/MastodonErrorResponse.java
@@ -32,23 +32,35 @@ public class MastodonErrorResponse extends ErrorResponse{
 			this.messageResource=R.string.connection_timed_out;
 		}else if(exception instanceof JsonSyntaxException || exception instanceof JsonIOException || httpStatus>=500){
 			this.messageResource=R.string.server_error;
-		}else if(httpStatus == 404){
+		}else if(httpStatus==404){
 			this.messageResource=R.string.not_found;
 		}else{
-			this.messageResource=R.string.unknown_error;
+			this.messageResource=0;
 		}
 	}
 
 	@Override
 	public void bindErrorView(View view){
 		TextView text=view.findViewById(R.id.error_text);
-		text.setText(view.getContext().getString(messageResource, error));
+		String message;
+		if(messageResource>0){
+			message=view.getContext().getString(messageResource, error);
+		}else{
+			message=error;
+		}
+		text.setText(message);
 	}
 
 	@Override
 	public void showToast(Context context){
 		if(context==null)
 			return;
-		Toast.makeText(context, context.getString(messageResource, error), Toast.LENGTH_SHORT).show();
+		String message;
+		if(messageResource>0){
+			message=context.getString(messageResource, error);
+		}else{
+			message=error;
+		}
+		Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
 	}
 }
