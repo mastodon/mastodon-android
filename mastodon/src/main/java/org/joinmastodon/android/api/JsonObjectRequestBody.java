9456e7555322b0a2882c909c6e99ed45e873a057
Grishka <git@grishka.me>
2025-08-18 17:51:02 +0300

    Buffer JSON request bodies to know the length in advance
    fixes #1008

--- mastodon/src/main/java/org/joinmastodon/android/api/JsonObjectRequestBody.java
+++ mastodon/src/main/java/org/joinmastodon/android/api/JsonObjectRequestBody.java
@@ -3,6 +3,7 @@ package org.joinmastodon.android.api;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonIOException;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
@@ -12,10 +13,21 @@ import okhttp3.RequestBody;
 import okio.BufferedSink;
 
 public class JsonObjectRequestBody extends RequestBody{
-	private final Object obj;
+	private final byte[] body;
 
-	public JsonObjectRequestBody(Object obj){
-		this.obj=obj;
+	public JsonObjectRequestBody(Object obj) throws IOException{
+		try{
+			ByteArrayOutputStream out=new ByteArrayOutputStream();
+			OutputStreamWriter writer=new OutputStreamWriter(out, StandardCharsets.UTF_8);
+			if(obj instanceof JsonElement)
+				writer.write(obj.toString());
+			else
+				MastodonAPIController.gson.toJson(obj, writer);
+			writer.flush();
+			body=out.toByteArray();
+		}catch(JsonIOException x){
+			throw new IOException(x);
+		}
 	}
 
 	@Override
@@ -24,16 +36,12 @@ public class JsonObjectRequestBody extends RequestBody{
 	}
 
 	@Override
+	public long contentLength() throws IOException{
+		return body.length;
+	}
+
+	@Override
 	public void writeTo(BufferedSink sink) throws IOException{
-		try{
-			OutputStreamWriter writer=new OutputStreamWriter(sink.outputStream(), StandardCharsets.UTF_8);
-			if(obj instanceof JsonElement)
-				writer.write(obj.toString());
-			else
-				MastodonAPIController.gson.toJson(obj, writer);
-			writer.flush();
-		}catch(JsonIOException x){
-			throw new IOException(x);
-		}
+		sink.write(body);
 	}
 }
