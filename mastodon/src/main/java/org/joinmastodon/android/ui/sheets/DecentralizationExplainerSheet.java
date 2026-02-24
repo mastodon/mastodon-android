1729b7bc1139dd7fef9308f574e1fbb3b729c218
Grishka <git@grishka.me>
2026-02-24 17:06:21 +0300

    Make the decentralization explainer a sheet again

--- mastodon/src/main/java/org/joinmastodon/android/ui/sheets/DecentralizationExplainerSheet.java
+++ mastodon/src/main/java/org/joinmastodon/android/ui/sheets/DecentralizationExplainerSheet.java
@@ -1,8 +1,9 @@
-package org.joinmastodon.android.ui.tooltips;
+package org.joinmastodon.android.ui.sheets;
 
 import android.content.Context;
 import android.text.style.TypefaceSpan;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.TextView;
 
 import org.joinmastodon.android.R;
@@ -10,21 +11,15 @@ import org.joinmastodon.android.api.session.AccountSessionManager;
 import org.joinmastodon.android.model.Account;
 import org.joinmastodon.android.ui.utils.UiUtils;
 
-public class DecentralizationExplainerTooltip extends BaseRichTooltip{
-	private final Account account;
-	private final String accountID;
+import androidx.annotation.NonNull;
 
-	public DecentralizationExplainerTooltip(Context context, Account account, String accountID){
+public class DecentralizationExplainerSheet extends M3BottomSheet{
+	public DecentralizationExplainerSheet(@NonNull Context context, String accountID, Account account){
 		super(context);
-		this.account=account;
-		this.accountID=accountID;
-	}
-
-	@Override
-	protected void initView(){
-		contentView=LayoutInflater.from(context).inflate(R.layout.profile_decentralization_tooltip, null);
-		TextView username=contentView.findViewById(R.id.username);
-		TextView domain=contentView.findViewById(R.id.domain);
+		View content=context.getSystemService(LayoutInflater.class).inflate(R.layout.sheet_decentralization_info, null);
+		setContentView(content);
+		TextView username=findViewById(R.id.username);
+		TextView domain=findViewById(R.id.domain);
 		username.setText(UiUtils.substituteStringWithSpan(context, R.string.handle_explanation_username, account.username, new TypefaceSpan("sans-serif-medium")));
 		String accountDomain=account.getDomain();
 		if(accountDomain==null)
