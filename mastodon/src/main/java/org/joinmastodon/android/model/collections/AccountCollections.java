10b2980e1eea3ec8909c6259df2069d2c3062031
Grishka <git@grishka.me>
2026-06-09 20:54:40 +0300

    Fix exception

--- mastodon/src/main/java/org/joinmastodon/android/model/collections/AccountCollections.java
+++ mastodon/src/main/java/org/joinmastodon/android/model/collections/AccountCollections.java
@@ -1,14 +1,16 @@
 package org.joinmastodon.android.model.collections;
 
 import org.joinmastodon.android.api.ObjectValidationException;
+import org.joinmastodon.android.api.RequiredField;
 import org.joinmastodon.android.model.BaseModel;
 import org.joinmastodon.android.model.PartialAccount;
 
 import java.util.List;
 
 public class AccountCollections extends BaseModel{
+	@RequiredField
 	public List<AccountCollection> collections;
-	public List<PartialAccount> partialAccounts;
+	public List<PartialAccount> partialAccounts=List.of();
 
 	@Override
 	public void postprocess() throws ObjectValidationException{
